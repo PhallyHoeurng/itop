007c3f5dfc4884c424182d598e32d38f6cf49db0
Hoeurng Phall <phally.hoeurng@neocomisp.com>
2026-05-20 15:11:47 +0700

    feature access level group

--- extensions/sample-extension/en.dict.sample-extension.php
+++ extensions/sample-extension/en.dict.sample-extension.php
@@ -30,7 +30,27 @@ Dict::Add('EN US', 'English', 'English', array(
     // UserRequest attribute overrides
     'Class:UserRequest/Attribute:access_level_name' => 'System Role',
     'Class:UserRequest/Attribute:access_level_name+' => 'Selected system role',
+    'Class:UserRequest/Attribute:access_level_id' => 'Access Level ID',
 
+    'Class:AccessLevelGroup' => 'Access Level Group',
+    'Class:AccessLevelGroup+' => 'Group of Access Levels',
+    'Class:AccessLevelGroup/Attribute:group_name' => 'Group Name',
+    'Class:AccessLevelGroup/Attribute:subcategory_id' => 'Service Subcategory',
+    'Class:AccessLevelGroup/Attribute:subcategory_name' => 'Service Subcategory',
+    'Class:AccessLevelGroup/Attribute:status' => 'Status',
+    'Class:AccessLevelGroup/Attribute:access_levels_list' => 'Access Levels',
+    'Class:AccessLevelGroup/Error:NoAccessLevel' => 'You must add at least one Access Level before creating this Access Level Group.',
+
+    'Class:ServiceSubcategory/Attribute:access_level_groups_list' => 'Access Level Groups',
+
+    'Class:AccessLevel/Attribute:access_level_group_id' => 'Access Level Group',
+    'Class:AccessLevel/Attribute:access_level_group_name' => 'Access Level Group',
+
+    'Class:UserRequestAccessLevel' => 'User Request Access Level',
+    'Class:UserRequestAccessLevel+' => 'Store User Request Access Level',
+    'Class:UserRequestAccessLevel/Attribute:name' => 'Name',
+    'Class:UserRequestAccessLevel/Attribute:request_id' => 'Request',
+    'Class:UserRequestAccessLevel/Attribute:accesslevel_id' => 'Access Level ID',
 ));
 
 
