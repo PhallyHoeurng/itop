7ed5edbb3e7b9d8fbf11e01022ef98f38663ebc3
Hoeurng Phall <phally.hoeurng@neocomisp.com>
2026-06-17 15:52:14 +0700

    show field in form chnage

--- extensions/sample-extension/en.dict.sample-extension.php
+++ extensions/sample-extension/en.dict.sample-extension.php
@@ -27,30 +27,66 @@ Dict::Add('EN US', 'English', 'English', array(
 
 
 
+//     // UserRequest attribute overrides
+//     'Class:UserRequest/Attribute:access_level_name' => 'System Role',
+//     'Class:UserRequest/Attribute:access_level_name+' => 'Selected system role',
+//     'Class:UserRequest/Attribute:access_level_id' => 'Access Level ID',
+
+//     'Class:AccessLevelGroup' => 'Access Level Group',
+//     'Class:AccessLevelGroup+' => 'Group of Access Levels',
+//     'Class:AccessLevelGroup/Attribute:group_name' => 'Group Name',
+//     'Class:AccessLevelGroup/Attribute:subcategory_id' => 'Service Subcategory',
+//     'Class:AccessLevelGroup/Attribute:subcategory_name' => 'Service Subcategory',
+//     'Class:AccessLevelGroup/Attribute:status' => 'Status',
+//     'Class:AccessLevelGroup/Attribute:access_levels_list' => 'Access Levels',
+//     'Class:AccessLevelGroup/Error:NoAccessLevel' => 'You must add at least one Access Level before creating this Access Level Group.',
+
+//     'Class:ServiceSubcategory/Attribute:access_level_groups_list' => 'Access Level Groups',
+
+//     'Class:AccessLevel/Attribute:access_level_group_id' => 'Access Level Group',
+//     'Class:AccessLevel/Attribute:access_level_group_name' => 'Access Level Group',
+
+//     'Class:UserRequestAccessLevel' => 'User Request Access Level',
+//     'Class:UserRequestAccessLevel+' => 'Store User Request Access Level',
+//     'Class:UserRequestAccessLevel/Attribute:name' => 'Name',
+//     'Class:UserRequestAccessLevel/Attribute:request_id' => 'Request',
+//     'Class:UserRequestAccessLevel/Attribute:accesslevel_id' => 'Access Level ID',
+
     // UserRequest attribute overrides
     'Class:UserRequest/Attribute:access_level_name' => 'System Role',
     'Class:UserRequest/Attribute:access_level_name+' => 'Selected system role',
     'Class:UserRequest/Attribute:access_level_id' => 'Access Level ID',
 
+    // Access Level Group
     'Class:AccessLevelGroup' => 'Access Level Group',
     'Class:AccessLevelGroup+' => 'Group of Access Levels',
     'Class:AccessLevelGroup/Attribute:group_name' => 'Group Name',
     'Class:AccessLevelGroup/Attribute:subcategory_id' => 'Service Subcategory',
     'Class:AccessLevelGroup/Attribute:subcategory_name' => 'Service Subcategory',
     'Class:AccessLevelGroup/Attribute:status' => 'Status',
-    'Class:AccessLevelGroup/Attribute:access_levels_list' => 'Access Levels',
-    'Class:AccessLevelGroup/Error:NoAccessLevel' => 'You must add at least one Access Level before creating this Access Level Group.',
+    // Updated list pointer and error string to reference Role Systems instead of Access Levels
+    'Class:AccessLevelGroup/Attribute:role_systems_list' => 'Role Systems',
+    'Class:AccessLevelGroup/Attribute:role_systems_list+' => 'List of roles associated with this access level group',
+    'Class:AccessLevelGroup/Error:NoRoleSystem' => 'You must add at least one Role System before creating this Access Level Group.',
 
+    // Service Subcategory
     'Class:ServiceSubcategory/Attribute:access_level_groups_list' => 'Access Level Groups',
 
-    'Class:AccessLevel/Attribute:access_level_group_id' => 'Access Level Group',
-    'Class:AccessLevel/Attribute:access_level_group_name' => 'Access Level Group',
+    // NEW NEW NEW: Role System Class Strings
+    'Class:RoleSystem' => 'Role System',
+    'Class:RoleSystem+' => 'System roles assigned to access groups',
+    'Class:RoleSystem/Attribute:role_name' => 'Role Name',
+    'Class:RoleSystem/Attribute:role_name+' => 'The unique name of this system role',
+    'Class:RoleSystem/Attribute:access_level_group_id' => 'Access Level Group',
+    'Class:RoleSystem/Attribute:access_level_group_id+' => 'Linked group for this system role',
+    'Class:RoleSystem/Attribute:access_level_group_name' => 'Access Level Group Name',
+    'Class:RoleSystem/Attribute:access_level_group_name+' => '',
 
+    // User Request Access Level
     'Class:UserRequestAccessLevel' => 'User Request Access Level',
     'Class:UserRequestAccessLevel+' => 'Store User Request Access Level',
     'Class:UserRequestAccessLevel/Attribute:name' => 'Name',
     'Class:UserRequestAccessLevel/Attribute:request_id' => 'Request',
     'Class:UserRequestAccessLevel/Attribute:accesslevel_id' => 'Access Level ID',
 ));
-
 ?>
